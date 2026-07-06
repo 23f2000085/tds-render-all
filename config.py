2e115755e4c787a8946960c54e4ed79fe788f963
23f2000085 <23f2000085@ds.study.iitm.ac.in>
2026-07-06 20:41:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+EMAIL = "23f2000085@ds.study.iitm.ac.in"
+
+Q1_ALLOWED_ORIGIN = "https://dash-95exn9.example.com"
+
+ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-jqgkyznt.apps.exam.local"
+
+PUBLIC_KEY_PEM = """YOUR PUBLIC KEY"""
+
+Q3_PORT = 8340
+Q3_WORKERS = 1
+Q3_DEBUG = False
+Q3_LOG_LEVEL = "error"
+
+Q5_API_KEY = "ak_0o36tbq7udcsxf005czvlks2"
+
+Q9_TOTAL_ORDERS = 46
+Q9_RATE_LIMIT = 17
+
+Q10_ALLOWED_ORIGIN = "https://app-ziiy19.example.com"
+Q10_RATE_LIMIT = 11
+
+EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
